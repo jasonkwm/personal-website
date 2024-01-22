922c2940f28e2e544c21960a80140ce4739322f7
Jason <jasonkwm13@gmail.com>
2024-01-22 18:13:18 +0800

    feat: update a about me in hero section and add prettier rc and create project section file.

---
+++ app/_components/ProjectSection.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const projects = [
+    {
+        image: require("../assets/personal_website.png"),
+        h3: "Personal Website",
+        p: "Build using React Typescript & Tailwind CSS",
+        link: "https://github.com/jasonkwm/personal-website",
+    },
+    {
+        image: require("../assets/minishell.gif"),
+        h3: "Minishell",
+        p: "By creating your very own shell. Things to learn in this project includes, tokenizing input, parsing input, forking processes and file descriptors.",
+        link: "https://github.com/jasonkwm/minishell",
+    },
+    {
+        image: require("../assets/so_long.gif"),
+        h3: "So Long",
+        p: "Learn all about textures, sprites, and some other very basic gameplay elements by creating a 2D game in C programming language using the MiniLibX library.",
+        link: "https://github.com/jasonkwm/so_long",
+    },
+    {
+        image: require("../assets/crowd-funding-bamboo.jpg"),
+        h3: "Crowdfunding Product Page",
+        p: "Practising the basics of front-end by using HTML, SCSS and Javascript this project include building a responsive web design and creating a modal.",
+        link: "https://github.com/jasonkwm/crowdfunding-product-page-main",
+    },
+    {
+        image: require("../assets/philo.png"),
+        h3: "Philosophers",
+        p: "Learn the basics of threading a process and managing memory access among threads by using Mutex.",
+        link: "https://github.com/jasonkwm/philo42",
+    },
+];
+
+const ProjectSection = () => {};
+
+export default ProjectSection;
