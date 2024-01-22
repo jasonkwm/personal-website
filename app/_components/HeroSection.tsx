806397bf89200f9394c037316e1a5fd9ba5c59a6
Jason <jasonkwm13@gmail.com>
2024-01-22 16:05:58 +0800

    chore: hero section file

---
+++ app/_components/HeroSection.tsx
@@ -0,0 +1,27 @@
+let socials = [
+    {
+        link: "https://github.com/jasonkwm",
+        img: require("../assets/github-sign.png"),
+        alt: "Github icons created by Dave Gandy",
+        id: 0xaa,
+    },
+    {
+        link: "https://www.linkedin.com/in/jason-kwm",
+        img: require("../assets/linkedin.png"),
+        alt: "Linkedin icons created by Freepik",
+        id: 0xbb,
+    },
+    {
+        link: "https://www.facebook.com/JasonKWM/",
+        img: require("../assets/facebook.png"),
+        alt: "Facebook icons created by Freepik",
+        id: 0xcc,
+    },
+    {
+        link: "https://www.instagram.com/jasonkwm/",
+        img: require("../assets/instagram.png"),
+        alt: "Instagram icons created by edt.im",
+        id: 0xdd,
+    },
+];
+const HeroSection = () => {};
