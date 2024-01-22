2a95c1ba34ab35258819d5bbaa9d856f4328e9ce
Jason <jasonkwm13@gmail.com>
2024-01-22 17:11:54 +0800

    feat: done hero section hopefully

--- app/_components/HeroSection.tsx
+++ app/_components/HeroSection.tsx
@@ -39,13 +39,13 @@ const HeroSection = () => {
             <div>
                 <a
                     className="inline-block px-12 py-5 mx-4 my-2 bg-black rounded-lg text-white text-sm font-semibold tracking-wider active:animate-expandButton hover:animate-expandButton transition-all duration-300"
-                    href="./#projects"
+                    href="#achievements"
                 >
                     ACHIEVEMENTS
                 </a>
                 <a
                     className="inline-block px-16 py-5 mx-4 my-2 bg-black rounded-lg text-white text-sm font-semibold tracking-wider active:animate-expandButton hover:animate-expandButton transition-all duration-300"
-                    href="./#projects"
+                    href="#projects"
                 >
                     PROJECTS
                 </a>
