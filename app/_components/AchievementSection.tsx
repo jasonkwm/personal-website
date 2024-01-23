4306ab911401089e7b44922e84249c3634206155
Jason <jasonkwm13@gmail.com>
2024-01-23 18:08:56 +0800

    feat: add section cover

--- app/_components/AchievementSection.tsx
+++ app/_components/AchievementSection.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import SectionHeading from './SectionHeading';
 const AchievementSection = () => {
 	return (
-		<SectionHeading
-			heading="ACHIEVEMENTS"
-			divider={true}
-			description="Browse through my hackathon journey, where each achievement reflects not just wins but the invaluable lessons learned in the heat of coding challenges."
-		/>
+		<section id="achievements" className="bg-[#fafafa] py-20">
+			<SectionHeading
+				heading="ACHIEVEMENTS"
+				divider={true}
+				description="Browse through my hackathon journey, where each achievement reflects not just wins but the invaluable lessons learned in the heat of coding challenges."
+			/>
+		</section>
 	);
 };
 
