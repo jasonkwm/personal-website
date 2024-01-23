9ea51a911dbac86a7260168dda86da0eab2683a5
Jason <jasonkwm13@gmail.com>
2024-01-23 08:38:33 +0800

    feat: add contact section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image';
 import HeroSection from './_components/HeroSection';
 import ProjectSection from './_components/ProjectSection';
+import ContactSection from './_components/ContactSection';
 export default function Home() {
 	return (
 		<main>
 			<HeroSection />
 			<ProjectSection />
+			<ContactSection />
 		</main>
 	);
 }
