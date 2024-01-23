cdd8518994cb17acca0d82f6b32b139fbbc461a2
Jason <jasonkwm13@gmail.com>
2024-01-23 18:00:53 +0800

    feat: start of achievements section

---
+++ app/_components/AchievementSection.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SectionHeading from './SectionHeading';
+const AchievementSection = () => {
+	return (
+		<SectionHeading
+			heading="ACHIEVEMENTS"
+			divider={true}
+			description="Explore my hackathon journey, highlighting achievements and the valuable experiences gained in each collaborative coding challenge."
+		/>
+	);
+};
+
+export default AchievementSection;
