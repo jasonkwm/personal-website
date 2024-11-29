64d9d8d8e2c9167bed5dcd78a5ef7742330018fa
Jason <jasonkwm13@gmail.com>
2024-11-29 15:41:56 +0800

    feat: add ethkl 2024 to achievements

--- app/_components/AchievementSection.tsx
+++ app/_components/AchievementSection.tsx
@@ -7,6 +7,7 @@ type ImageType = {
 	width: number;
 	height: number;
 	alt: string;
+	fill?: boolean;
 };
 
 type RewardImageType = {
@@ -33,27 +34,28 @@ const imgObj: ImageType = {
 
 const achievements: ItemType[] = [
 	{
-		id: 0xaa,
-		heading: 'Ethereum Kuala Lumpur 2023',
-		description:
-			'Our winning entry was a fully functional dApp that showcased the incredible Zero Knowledge capabilities of Aleo.',
+		id: 0xa3,
+		heading: 'Ethereum Kuala Lumpur 2024',
+		description: '42Hour Hackathon + 42KM Marathon? Sign me up!',
 		pageLink:
-			'https://www.linkedin.com/posts/jasonkwm_hackathon-aleo-ethkl-activity-7120019021671927808-CZUE?utm_source=share&utm_medium=member_desktop',
-		projectLink: 'https://github.com/jasonkwm/leo-zakkarat',
+			'https://www.linkedin.com/posts/jasonkwm_klscm-ethkl-42kl-activity-7248973321864634368-5Uma/?utm_source=share&utm_medium=member_desktop',
+		projectLink: 'https://devfolio.co/projects/crypto-survey-fa45',
 		image: {
-			path: '/achievements/ethkl23-hackathon/pitching.jpg',
+			path: '/achievements/ethkl24/medals.jpg',
 			width: 805.2,
 			height: 603.9,
-			alt: 'bunyod, jason, how zheng, and alvin pitching about our zakkarat project to the aleo team',
+			alt: 'medal of KLSCM and ETHKL24 pass',
+			fill: false,
 		},
 		rewards: [
 			{ path: '/rewards/gold-medal-freepik.png', title: 'First Place' },
+			{ path: '/rewards/running-flaticon.png', title: 'Full Marathon Finisher!' },
+			{ path: '/rewards/money-rain-flaticon.png', title: 'Won $800 USD' },
 			{ path: '/rewards/shipment-freepik.png', title: 'Swag/Merch Rewards' },
-			{ path: '/rewards/grant-freepik.png', title: 'Grants Offering' },
 		],
 	},
 	{
-		id: 0xbb,
+		id: 0xa2,
 		heading: '42KL GameJam 2023',
 		description:
 			"Our challenge was to create a game within 55 hours, incorporating the theme of 'Lost' and utilizing the Godot Engine.",
@@ -68,6 +70,27 @@ const achievements: ItemType[] = [
 		},
 		rewards: [
 			{ path: '/rewards/gold-medal-freepik.png', title: 'First Place' },
+			{ path: '/rewards/shipment-freepik.png', title: 'Swag/Merch Rewards' },
+		],
+	},
+	{
+		id: 0xa1,
+		heading: 'Ethereum Kuala Lumpur 2023',
+		description:
+			'Our winning entry was a fully functional dApp that showcased the incredible Zero Knowledge capabilities of Aleo.',
+		pageLink:
+			'https://www.linkedin.com/posts/jasonkwm_hackathon-aleo-ethkl-activity-7120019021671927808-CZUE?utm_source=share&utm_medium=member_desktop',
+		projectLink: 'https://github.com/jasonkwm/leo-zakkarat',
+		image: {
+			path: '/achievements/ethkl23-hackathon/pitching.jpg',
+			width: 805.2,
+			height: 603.9,
+			alt: 'bunyod, jason, how zheng, and alvin pitching about our zakkarat project to the aleo team',
+		},
+		rewards: [
+			{ path: '/rewards/gold-medal-freepik.png', title: 'First Place' },
+			{ path: '/rewards/grant-freepik.png', title: 'Grants Offering' },
+			{ path: '/rewards/money-rain-flaticon.png', title: 'Won $3000 USD' },
 			{ path: '/rewards/shipment-freepik.png', title: 'Swag/Merch Rewards' },
 		],
 	},
@@ -98,33 +121,34 @@ const AchievementItem = ({ item }: AchievementItemProps) => {
 	const buttonStyle =
 		'text-sm lg:text-base text-center px-5 py-3 text-white bg-black rounded-xl hover:scaleHover scaleHoverT';
 	return (
-		<article className="flex grow flex-col max-w-md rounded-lg shadow-lg hover:shadow-2xl transition-all duration-300 ">
+		<article className="flex grow flex-col max-w-md rounded-lg shadow-lg hover:shadow-2xl transition-all duration-300">
 			<Image
 				src={item.image.path}
 				width={item.image.width}
 				height={item.image.height}
 				alt={item.image.alt}
-				className="h-2/3 rounded-t-lg"
+				className={`rounded-t-lg object-cover max-h-[340px]`}
 				priority
 			/>
-			<div className="py-12 px-6 flex flex-col gap-4">
-				<h2 className="font-bold text-2xl">{item.heading}</h2>
-				<p className="text-[#666] lg:text-lg w-11/12">{item.description}</p>
-				<div className="mb-8 flex items-center gap-3">
-					<h3 className="font-bold">Rewards:</h3>
-					{item.rewards.map((reward, index) => (
-						<Image
-							key={index}
-							src={reward.path}
-							width={46}
-							height={46}
-							alt={reward.title}
-							title={reward.title}
-						/>
-					))}
+			<div className="py-12 px-6 flex flex-col gap-4 h-full justify-between">
+				<div className="flex flex-col gap-4">
+					<h2 className="font-bold text-2xl">{item.heading}</h2>
+					<p className="text-[#666] lg:text-lg w-11/12">{item.description}</p>
 				</div>
-
-				<div className="flex flex-col self-center gap-3 w-11/12 lg:w-10/12">
+				<div className="flex flex-col gap-3 w-11/12 lg:w-10/12">
+					<div className="flex items-center gap-3">
+						<h3 className="font-bold">Rewards:</h3>
+						{item.rewards.map((reward, index) => (
+							<Image
+								key={index}
+								src={reward.path}
+								width={46}
+								height={46}
+								alt={reward.title}
+								title={reward.title}
+							/>
+						))}
+					</div>
 					<a
 						href={item.pageLink}
 						className={`${buttonStyle}`}
